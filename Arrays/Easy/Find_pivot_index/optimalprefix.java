5d5f024ee39c24e2c893b1822d142175e190d658
Jithin <jithins2901@gmail.com>
2026-09-19 09:34:30 +0530

    Solution for new problem

---
+++ Arrays/Easy/Find_pivot_index/optimalprefix.java
@@ -0,0 +1,19 @@
+class Solution{
+    public int pivotIndex(int[] nums){
+        int n=nums.length;
+        int rightsum=0;
+        for(int i=0;i<n;i++){
+            rightsum+=nums[i];
+        }   
+        int leftsum=0;
+        for(int i=0;i<n;i++){
+            rightsum-=nums[i];
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+        // tc -> O(n) sc -> O(1)
+    }
+}
